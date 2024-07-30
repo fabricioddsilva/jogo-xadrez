8e36abcb29b163a1ccbf05ccaf01dc6671785a5e
Fabricio Dias <fabriciodsilva3404@gmail.com>
2024-07-30 10:08:53 -0300

    Movimento de peças

--- src/app/Main.java
+++ src/app/Main.java
@@ -4,10 +4,27 @@ import boardgame.Board;
 import boardgame.Position;
 import chess.ChessMatch;
 import chess.ChessPiece;
+import chess.ChessPosition;
+
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         ChessMatch match = new ChessMatch();
-        UI.printBoard(match.getPieces());
+
+        while(true){
+            UI.printBoard(match.getPieces());
+            System.out.println();
+            System.out.print("Source: ");
+            ChessPosition source = UI.readChessPosition(sc);
+
+            System.out.println();
+            System.out.print("Target: ");
+            ChessPosition target = UI.readChessPosition(sc);
+
+            ChessPiece capturedPiece = match.performChessMove(source, target);
+        }
+
     }
 }
