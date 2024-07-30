d48e8faf89b7bc75848d9f6fe2c4974df6fc0901
Fabricio Dias <fabriciodsilva3404@gmail.com>
2024-07-30 18:00:50 -0300

    Manipulando peças capturadas

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,24 +1,25 @@
 package app;
 
-import boardgame.Board;
-import boardgame.Position;
 import chess.ChessException;
 import chess.ChessMatch;
 import chess.ChessPiece;
 import chess.ChessPosition;
 
+import java.util.ArrayList;
 import java.util.InputMismatchException;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         ChessMatch match = new ChessMatch();
+        List<ChessPiece> captured = new ArrayList<>();
 
         while(true){
             try {
                 UI.clearScreen();
-                UI.printMatch(match);
+                UI.printMatch(match, captured);
                 System.out.println();
                 System.out.print("Source: ");
                 ChessPosition source = UI.readChessPosition(sc);
@@ -32,6 +33,10 @@ public class Main {
                 ChessPosition target = UI.readChessPosition(sc);
 
                 ChessPiece capturedPiece = match.performChessMove(source, target);
+
+                if(capturedPiece != null){
+                    captured.add(capturedPiece);
+                }
             }
             catch (ChessException e){
                 System.out.println(e.getMessage());
