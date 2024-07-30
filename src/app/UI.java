0e09ad40a9bb785357cd1135b2cc51cc0ca5f0c7
Fabricio Dias <fabriciodsilva3404@gmail.com>
2024-07-30 09:49:26 -0300

    Melhoria na impressão do tabuleiro usando cores

--- src/app/UI.java
+++ src/app/UI.java
@@ -1,8 +1,31 @@
 package app;
 
 import chess.ChessPiece;
+import chess.Color;
 
 public class UI {
+
+
+    public static final String ANSI_RESET = "\u001B[0m";
+    public static final String ANSI_BLACK = "\u001B[30m";
+    public static final String ANSI_RED = "\u001B[31m";
+    public static final String ANSI_GREEN = "\u001B[32m";
+    public static final String ANSI_YELLOW = "\u001B[33m";
+    public static final String ANSI_BLUE = "\u001B[34m";
+    public static final String ANSI_PURPLE = "\u001B[35m";
+    public static final String ANSI_CYAN = "\u001B[36m";
+    public static final String ANSI_WHITE = "\u001B[37m";
+
+    public static final String ANSI_BLACK_BACKGROUND = "\u001B[40m";
+    public static final String ANSI_RED_BACKGROUND = "\u001B[41m";
+    public static final String ANSI_GREEN_BACKGROUND = "\u001B[42m";
+    public static final String ANSI_YELLOW_BACKGROUND = "\u001B[43m";
+    public static final String ANSI_BLUE_BACKGROUND = "\u001B[44m";
+    public static final String ANSI_PURPLE_BACKGROUND = "\u001B[45m";
+    public static final String ANSI_CYAN_BACKGROUND = "\u001B[46m";
+    public static final String ANSI_WHITE_BACKGROUND = "\u001B[47m";
+
+
     public static void printBoard(ChessPiece[][] pieces){
         //Percorrendo as linhas
         for (int i = 0; i < pieces.length; i++){
@@ -19,15 +42,20 @@ public class UI {
         //No final for imprimir a marcação de colunas
         System.out.println("  a b c d e f g h");
     }
+
     //Imprimir a peça
     private static void printPiece(ChessPiece piece){
-        //Caso a peça for nula imprimir "-", se não imprimir a peça
-        if (piece == null){
+        if (piece == null) {
             System.out.print("-");
-        } else {
-            System.out.print(piece);
         }
-        //No final independente da existência, da um espaço entre as peças
+        else {
+            if (piece.getColor() == Color.WHITE) {
+                System.out.print(ANSI_WHITE + piece + ANSI_RESET);
+            }
+            else {
+                System.out.print(ANSI_YELLOW + piece + ANSI_RESET);
+            }
+        }
         System.out.print(" ");
     }
 }
